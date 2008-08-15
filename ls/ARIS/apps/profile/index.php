3d41acf17beed3cf9ee3b8d7844c30ffa4fcbae6
djgagnon <djgagnon@9a1de14e-70bd-11de-9e9f-05296fd93cd0>
2008-08-15 00:25:46 +0000

    Debug session variable

--- ls/ARIS/apps/profile/index.php
+++ ls/ARIS/apps/profile/index.php
@@ -1,23 +1,23 @@
 <?php
 require_once('../../common.php');
 
-switch ($_SESSION['profile_page']) {
-	case 1:
-		include ('profile1.php');
-		break;
-	case 2:
-		include ('profile2.php');
-		break;
-	case 3:
-		include ('profile3.php');
-		break;
-	case 4:
-		include ('profile4.php');
-		break;
-	default:
-		include ('intro.php');
-		
+if (isset($_SESSION['profile_page'])) { 
+	switch ($_SESSION['profile_page']) {
+		case 1:
+			include ('profile1.php');
+			break;
+		case 2:
+			include ('profile2.php');
+			break;
+		case 3:
+			include ('profile3.php');
+			break;
+		case 4:
+			include ('profile4.php');
+			break;
+	}
 }
+else include ('intro.php');
 
 ?>
 </p>
