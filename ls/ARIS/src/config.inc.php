3c6379588797457e3a3745e81fcf30baa323b2e3
djgagnon <djgagnon@9a1de14e-70bd-11de-9e9f-05296fd93cd0>
2008-10-10 14:43:56 +0000

    importing editor

--- ls/ARIS/src/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// MySQL host name, user name, password, database, and table
-	$opts['hn'] = 'localhost';
-	$opts['un'] = 'arisuser';
-	$opts['pw'] = 'arispwd';
-	$opts['db'] = 'aris';
-	
-	
-?>
