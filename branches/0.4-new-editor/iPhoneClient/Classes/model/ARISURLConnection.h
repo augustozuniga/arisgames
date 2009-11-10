bb3245763b8b66e4af7320cda9cbaa6dbc860af6
bcdeith <bcdeith@9a1de14e-70bd-11de-9e9f-05296fd93cd0>
2009-11-10 21:46:33 +0000

    Add Subclass of NSURLConnection, which has property of which parser to use. Used for async requests. Completion routine gets parser selector from subclass

---
+++ branches/0.4-new-editor/iPhoneClient/Classes/model/ARISURLConnection.h
@@ -0,0 +1,19 @@
+//
+//  ARISURLConnection.h
+//  ARIS
+//
+//  Created by Brian Deith on 11/10/09.
+//  Copyright 2009 Brian Deith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ARISURLConnection : NSURLConnection {
+	
+	SEL parser;
+}
+
+@property(readwrite) SEL parser;
+
+@end
---
+++ branches/0.4-new-editor/iPhoneClient/Classes/model/ARISURLConnection.m
@@ -0,0 +1,17 @@
+//
+//  ARISURLConnection.m
+//  ARIS
+//
+//  Created by Brian Deith on 11/10/09.
+//  Copyright 2009 Brian Deith. All rights reserved.
+//
+
+#import "ARISURLConnection.h"
+
+
+@implementation ARISURLConnection
+@synthesize parser;
+
+
+
+@end
