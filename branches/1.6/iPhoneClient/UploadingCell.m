c328b0ebbff15c2cbbef4aa9559f1c7f21adc976
mrdavidgagnon@gmail.com <mrdavidgagnon@gmail.com@9a1de14e-70bd-11de-9e9f-05296fd93cd0>
2012-04-15 22:42:23 +0000

    branching 1.6

---
+++ branches/1.6/iPhoneClient/UploadingCell.m
@@ -0,0 +1,13 @@
+//
+//  UploadingCell.m
+//  ARIS
+//
+//  Created by Brian Thiel on 1/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UploadingCell.h"
+
+@implementation UploadingCell
+@synthesize progressBar,updatingLabel;
+@end
