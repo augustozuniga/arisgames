3f938d1854b1416b95cb386dfc1f7a78a1393828
mrdavidgagnon <mrdavidgagnon@9a1de14e-70bd-11de-9e9f-05296fd93cd0>
2012-08-06 19:32:27 +0000

    Tagging 1.7.1

---
+++ tags/1.7.1 RC2/iPhoneClient/DialogViewController.h
@@ -0,0 +1,120 @@
+//
+//  aris_conversationViewController.h
+//  aris-conversation
+//
+//  Created by Kevin Harris on 09/11/17.
+//  Copyright Studio Tectorum 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SceneParser.h"
+#import "AsyncMediaImageView.h"
+#import "Node.h"
+#import "Npc.h"
+#import "Scene.h"
+#import "SceneParser.h"
+
+@interface DialogViewController : UIViewController<SceneParserDelegate, AsyncMediaImageViewDelegate, UIScrollViewDelegate, UITextFieldDelegate, AVAudioPlayerDelegate> {
+	IBOutlet	AsyncMediaImageView	*npcImage;
+	IBOutlet	AsyncMediaImageView	*pcImage;
+	IBOutlet	UIWebView	*npcWebView;
+	IBOutlet	UIWebView	*pcWebView;
+	IBOutlet	UITableView	*pcTableView;
+	IBOutlet	UITextField	*pcAnswerView;
+	IBOutlet	UIScrollView *npcScrollView;
+	IBOutlet	UIScrollView *pcScrollView;
+	IBOutlet	UIScrollView *npcImageScrollView;
+	IBOutlet	UIScrollView *pcImageScrollView;
+	IBOutlet	UIActivityIndicatorView *pcActivityIndicator;
+	IBOutlet	UILabel		*nothingElseLabel;
+	IBOutlet	UIButton	*pcContinueButton;
+	IBOutlet	UIButton	*npcContinueButton;
+	UIBarButtonItem	*textSizeButton;
+    UIBarButtonItem	*specialBackButton;
+	
+	IBOutlet	UIView	*mainView;
+	IBOutlet	UIView	*npcView;
+	IBOutlet	UIView	*pcView;
+    IBOutlet	UIScrollView	*npcVideoView;
+	
+	IBOutlet	UITableViewController	*pcTableViewController;
+	NSString	*resourcePath;
+	
+	NSArray			*currentScript;
+	NSInteger		scriptIndex;
+	NSInteger		lastPcId;
+	NSInteger		lastNpcId;
+	Scene			*cachedScene;
+    NSString        *exitToTabVal;
+	UIView			*cachedScrollView;
+	SceneParser		*parser;
+		
+	Npc				*currentNpc;
+	Node			*currentNode;
+	NSArray			*optionList;
+
+    UILabel         *lbl;
+	BOOL			closingScriptPlaying;
+	int			    textboxSize;
+    BOOL            hideLeaveConversationButton;
+    
+    AVAudioPlayer *player;
+    ARISMoviePlayerViewController *ARISMoviePlayer;
+    
+    UIActivityIndicatorView *waiting;
+}
+
+
+@property(nonatomic)UILabel *lbl;
+@property(nonatomic) IBOutlet AsyncMediaImageView	*npcImage;
+@property(nonatomic) IBOutlet AsyncMediaImageView	*pcImage;
+@property(nonatomic) IBOutlet UIWebView		*npcWebView;
+@property(nonatomic) IBOutlet UIWebView		*pcWebView;
+@property(nonatomic) IBOutlet UITableView	*pcTableView;
+@property(nonatomic) IBOutlet UITextField	*pcAnswerView;
+@property(nonatomic) IBOutlet UILabel		*nothingElseLabel;
+@property(nonatomic) IBOutlet UIButton *npcContinueButton;
+@property(nonatomic) IBOutlet UIButton *pcContinueButton;
+@property(nonatomic) IBOutlet UIBarButtonItem *textSizeButton;
+@property(nonatomic) IBOutlet UIBarButtonItem *specialBackButton;
+@property (nonatomic, strong) AVAudioPlayer *player;
+@property (nonatomic, strong) ARISMoviePlayerViewController *ARISMoviePlayer;
+@property (nonatomic)         UIActivityIndicatorView *waiting;
+@property                     BOOL closingScriptPlaying;
+@property                     int textboxSize;
+@property                     BOOL            hideLeaveConversationButton;
+
+@property(nonatomic) IBOutlet NSString *exitToTabVal;
+
+@property(nonatomic) IBOutlet UIScrollView	*npcScrollView;
+@property(nonatomic) IBOutlet UIScrollView	*pcScrollView;
+@property(nonatomic) IBOutlet UIScrollView	*npcImageScrollView;
+@property(nonatomic) IBOutlet UIScrollView	*pcImageScrollView;
+@property(nonatomic) IBOutlet UIActivityIndicatorView *pcActivityIndicator;
+
+@property(nonatomic) Npc *currentNpc;
+@property(nonatomic) Node *currentNode;
+
+@property(nonatomic) IBOutlet UIView		*mainView;
+@property(nonatomic) IBOutlet UIView		*npcView;
+@property(nonatomic) IBOutlet UIView		*pcView;
+@property(nonatomic) IBOutlet UIScrollView		*npcVideoView;
+
+- (IBAction)continueButtonTouchAction;
+- (IBAction)npcScrollerTouchAction;
+
+- (void) beginWithNPC:(Npc *)aNpc;
+- (void) loadNPCImage:(NSInteger)mediaId;
+- (void) continueScript;
+- (void) didFinishParsing;
+- (void) applyNPCWithGreeting;
+- (void) applyPlayerOptions;
+- (void) finishApplyingPlayerOptions:(NSArray*)options;
+- (void) showWaitingIndicatorForPlayerOptions;
+- (void) dismissWaitingIndicatorForPlayerOptions;
+- (void) imageFinishedLoading;
+- (void) playAudioOrVideoFromMedia:(Media*)media andHidden:(BOOL)hidden;
+- (void) MPMoviePlayerLoadStateDidChangeNotification:(NSNotification *)notif;
+
+@end
+
---
+++ tags/1.7.1 RC2/iPhoneClient/LoadingViewController.m
@@ -0,0 +1,74 @@
+//
+//  LoadingViewControllerViewController.m
+//  ARIS
+//
+//  Created by Brian Thiel on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+#import "AppModel.h"
+#import "ARISAppDelegate.h"
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+@synthesize splashImage, progressBar,progressLabel,receivedData, timer;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        receivedData = 0;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+   
+    progressBar.progress = 0.0;
+    [self performSelectorOnMainThread:@selector(moveProgressBar) withObject:nil waitUntilDone:YES];
+}
+-(float)receivedData{
+    return receivedData;
+}
+-(void)setReceivedData:(float)r{
+    receivedData = r;
+    [self performSelectorOnMainThread:@selector(moveProgressBar) withObject:nil waitUntilDone:YES];
+}
+- (void)moveProgressBar {
+    
+    float actual = (receivedData/(float)11);
+    if (actual < 1) {
+        progressBar.progress =actual;
+        [progressBar setNeedsLayout];
+        [progressBar setNeedsDisplay];
+        [progressLabel setNeedsDisplay];
+        [progressLabel setNeedsLayout];
+       
+    }
+    else if(actual == 1){
+           if ([AppModel sharedAppModel].currentGame.completedQuests < 1)
+               [[RootViewController sharedRootViewController] performSelector:@selector(displayIntroNode) withObject:nil afterDelay:.1];
+        [self dismissModalViewControllerAnimated:NO];
+        [RootViewController sharedRootViewController].loadingVC = nil;
+    
+} 
+}
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
